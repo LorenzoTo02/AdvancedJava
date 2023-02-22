b999f1709880c6362b0878050561eba6183e0574
LorenzoTo02 <lorenzozeppegno@gmail.com>
2023-02-22 14:57:20 +0100

    capitalize es

---
+++ Esercizi/src/capitalize/CapitalizeString.java
@@ -0,0 +1,36 @@
+package capitalize;
+
+
+
+public class CapitalizeString {
+
+	public static void main(String[] args) {
+		
+		String src = "mamma ho perso l'aereo e mio padre 50enne mi ha picchiato!";
+		
+		System.out.println(capitalizeFirstChars(src));
+		
+	}
+	
+	public static String capitalizeFirstChars(String src) {
+		
+		char[] charArray = src.toCharArray();
+		
+		for (int i = 0; i < charArray.length; i++) {
+			if(i != 0) {
+				if(Character.isLetter(charArray[i-1]) && Character.isLetter(charArray[i])) {
+					
+				} else {
+					charArray[i] = Character.toUpperCase(charArray[i]);
+				}
+			} else {
+				if(Character.isLetter(charArray[i])) {
+					charArray[i] = Character.toUpperCase(charArray[i]);
+				}
+			}
+		}
+		
+		return String.valueOf(charArray);
+	}
+
+}
